4f732e80b2ca2e3a8b89071fcb3dfd32d55b6042
Harsha Hegde <harshahegde10s@gmail.com>
2025-04-14 11:44:37 +0530

    Added Cart page and made somme changes

--- App.js
+++ App.js
@@ -1,22 +1,68 @@
-import React from 'react';
-import MenuPage from './MenuPage';
+import React, { useState } from "react";
+import MenuPage from "./MenuPage.js";
+import Cart from "./Cart.js";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
+export default function App() {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (item) => {
+    const exists = cart.find(
+      (i) =>
+        i.id === item.id && i.size === item.size && i.flavor === item.flavor
+    );
+    if (exists) {
+      setCart((prev) =>
+        prev.map((i) =>
+          i.id === item.id && i.size === item.size && i.flavor === item.flavor
+            ? { ...i, quantity: i.quantity + 1 }
+            : i
+        )
+      );
+    } else {
+      setCart((prev) => [...prev, { ...item, quantity: 1 }]);
+    }
+  };
+
+  const increment = (id) => {
+    setCart((prev) =>
+      prev.map((item) =>
+        item.id === id ? { ...item, quantity: item.quantity + 1 } : item
+      )
+    );
+  };
+
+  const decrement = (id) => {
+    setCart((prev) =>
+      prev.map((item) =>
+        item.id === id
+          ? { ...item, quantity: Math.max(1, item.quantity - 1) }
+          : item
+      )
+    );
+  };
+
+  const removeFromCart = (id) => {
+    setCart((prev) => prev.filter((item) => item.id !== id));
+  };
+
+  const updateCustomization = (id, field, value) => {
+    setCart((prev) =>
+      prev.map((item) => (item.id === id ? { ...item, [field]: value } : item))
+    );
+  };
+
   return (
-<<<<<<< HEAD
-    <div>
-      <MenuPage />
-=======
-    <div className="App">
+    <div className="container my-4">
+      <MenuPage addToCart={addToCart} />
 
-      <Button variant='danger'>GIT</Button>
-
-      <h1>Start the Ideal Cafe</h1>
-
->>>>>>> 6c23c5e8f62e488572c323aa93653a96c917394d
+      <Cart
+        cart={cart}
+        increment={increment}
+        decrement={decrement}
+        removeFromCart={removeFromCart}
+        updateCustomization={updateCustomization}
+      />
     </div>
   );
 }
-
-export default App;
-
