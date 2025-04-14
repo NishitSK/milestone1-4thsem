f2d645baa636c1223ac07ba7652ba9a3daeb0f01
NishitSK <138647749+NishitSK@users.noreply.github.com>
2025-04-14 21:43:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-const rootElement = document.getElementById("root");
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-
+root.render(<App />);
